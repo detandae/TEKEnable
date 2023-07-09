574596b927df3c8532e0f5d5b9fddfd6dd6e8b91
3AMLABS\adetari <andras.detari@lastpass.com>
2023-07-09 16:15:15 +0200

    Add Go Back button to Success page

--- Frontend/TEKEnable/src/app/success-message/success-message.component.ts
+++ Frontend/TEKEnable/src/app/success-message/success-message.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-success-message',
   templateUrl: './success-message.component.html',
@@ -8,11 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class SuccessMessageComponent implements OnInit {
 
   message: string;
-  constructor() { 
+  constructor( private router: Router) { 
     this.message="Your registration was successful!";
   }
 
   ngOnInit(): void {
   }
 
+  goBack():void{
+    this.router.navigate(['/']);
+  }
 }
